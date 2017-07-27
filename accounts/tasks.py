4e548e727c65e6b382366124063d0b9061c487b8
OllyDorvelus <ollydorvelus@gmail.com>
2017-07-27 17:34:43 -0400

    wontwork

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -3,18 +3,18 @@ from __future__ import absolute_import
 from celery import shared_task
 
 
-@shared_task
-def add(x, y):
-    print(x+y)
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
+# @shared_task
+# def add(x, y):
+#     print(x+y)
+#     return x + y
+#
+#
+# @shared_task
+# def mul(x, y):
+#     return x * y
+#
+#
+# @shared_task
+# def xsum(numbers):
+#     return sum(numbers)
 
