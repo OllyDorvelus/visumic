dc1aba2feb49f11f92e7bc7a588b261ab52c0d22
OllyDorvelus <ollydorvelus@gmail.com>
2017-07-26 20:02:52 -0400

    stylesheet

--- videos/models.py
+++ videos/models.py
@@ -266,9 +266,8 @@ class VideoModel(models.Model, HitCountMixin):
 # ready2party.save()
 # print(ready2party.input_video.url)
 def post_save_video_receiver(sender, instance, created, *args, **kwargs):
-    print("Tada")
-    # if created:
-    #     convert_video_to_mp4.delay(instance)
+    if created:
+        convert_video_to_mp4.delay(instance)
       #   #instance.input_video = instance.video
       #   video = instance.video.url.replace("/", "", 1)
       #   #video = os.path.abspath(instance.video.url)
