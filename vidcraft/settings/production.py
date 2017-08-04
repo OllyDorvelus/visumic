75112fab6d911ae3d7865077ead8ac8d713105a5
OllyDorvelus <ollydorvelus@gmail.com>
2017-08-04 16:41:13 -0400

    celsettings

--- vidcraft/settings/production.py
+++ vidcraft/settings/production.py
@@ -166,7 +166,7 @@ utcoffset()
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 # CELERY_TIMEZONE = 'EST'
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'America/Chicago'
 USE_TZ = True
 BROKER_URL = 'amqp://ysokecez:tjw61OHXjU49cOFff7oTWZWhDursUxOH@wasp.rmq.cloudamqp.com/ysokecez'
 #BROKER_URL = os.environ.get('CLOUDAMQP_URL')
@@ -178,9 +178,9 @@ CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
 CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 # For celery
 #CELERY_ENABLE_UTC = False
-TIME_ZONE = 'EST'
+TIME_ZONE = 'CDT'
 CELERY_TASK_RESULT_EXPIRES = None
-CELERY_TIMEZONE = 'America/New_York'
+CELERY_TIMEZONE = 'America/Chicago'
 USE_I18N = True
 
 USE_L10N = True
