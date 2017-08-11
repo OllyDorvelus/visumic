031705597ef1cc17b79a60bd12bdf6d121c6cde5
OllyDorvelus <ollydorvelus@gmail.com>
2017-08-11 14:12:57 -0400

    .mov

--- accounts/validators.py
+++ accounts/validators.py
@@ -12,7 +12,7 @@ def validate_file_video_extension(value):
     import os
     from django.core.exceptions import ValidationError
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.mp4', '.mkv', '.avi', '.flv', '.wmv', '.webm', '.mpeg']
+    valid_extensions = ['.mp4', '.mkv', '.avi', '.flv', '.wmv', '.webm', '.mpeg', '.mov']
     if not ext.lower() in valid_extensions:
         raise ValidationError(u'Unsupported file extension.')
 # def validate_file_extension(value):
