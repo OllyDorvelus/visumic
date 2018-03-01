5789bf521d7319fec28c78d29a00edf01f2c5fdc
OllyDorvelus <ollydorvelus@gmail.com>
2018-02-28 22:02:11 -0500

    fixed pagination

--- videos/api/pagination.py
+++ videos/api/pagination.py
@@ -3,7 +3,7 @@ __author__ = 'OllyD'
 from rest_framework import pagination
 
 class StandardResultsPagination(pagination.PageNumberPagination):
-    page_size = 8
+    page_size = 24
     page_size_query_param = 'page_size'
     max_page_size = 2000
 
